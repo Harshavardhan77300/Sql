dc1c4b482b3f6b4b08409537a67cfb62448f36ae
Harshavardhan V <93387161+Harshavardhan77300@users.noreply.github.com>
2024-12-14 11:21:35 +0530

    Update transcription.sql

--- transcription.sql
+++ transcription.sql
@@ -4,10 +4,10 @@ USE `transcriptions_db`;
 -- Table for Transcription schema
 DROP TABLE IF EXISTS `transcription`;
 CREATE TABLE `transcription` (
-  `consultationid` int NOT NULL,
-  `doctorid` int NOT NULL,
-  `patientid` int NOT NULL,
-  `transcription_text` text NOT NULL,
+  `consultationId` int NOT NULL,
+  `doctorId` int NOT NULL,
+  `patientId` int NOT NULL,
+  `transcriptionText` text NOT NULL,
   `timestamp` datetime NOT NULL,
   `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modifiedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
