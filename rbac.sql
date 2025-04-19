0ddf8ddf159d7f4af8aab8ea7c7eb8d7e60ab37c
Harshavardhan V <93387161+Harshavardhan77300@users.noreply.github.com>
2025-04-19 17:09:19 +0530

    Update rbac.sql

--- rbac.sql
+++ rbac.sql
@@ -1,4 +1,3 @@
-
 -- Clients Table
 DROP TABLE IF EXISTS `clients`;
 CREATE TABLE `clients` (
@@ -6,18 +5,6 @@ CREATE TABLE `clients` (
     `name` VARCHAR(50) UNIQUE NOT NULL,        -- Example: Fincuro, Ust, Iravx
     `type` VARCHAR(10) NOT NULL CHECK (type IN ('B2B', 'B2C')), -- Client type: B2B or B2C
     `description` TEXT,                        -- Optional description
-    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-);
-
-
--- Subjects Table (Renamed from User Subjects)
-DROP TABLE IF EXISTS `subjects`;
-CREATE TABLE `subjects` (
-    `id` SERIAL,
-    `subject` VARCHAR(50) NOT NULL,                           -- Subject: CEO, Director, etc.
-    `client_id` INT REFERENCES clients(id) ON DELETE CASCADE, -- Links subject to a client
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
